467f692bd8a46701f175ace16a4bde731d35d09f
João Vitor Ribeiro Pereira <joaovrp.mec.ufsc@gmail.com>
2022-08-26 15:23:44 -0300

    Third Version - Working Login Screen

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-Second version
+Third version
 
 Changelog:
 
-- Added Chat history linked with UOL API;
-- Automatic scrollbar.
+- Added working login screen;
 
 
 To Do:
 
-- Login;
 - Chat history according to username;
 - Enable sending messages and store them in the server;
 - On/off status;
 - Enable private messages;
-- Do the login screen (bonus);
 - Do the online people sidebar (bonus);
 - Enable sending messages with an Enter press (bonus).
--- script.js
+++ script.js
@@ -1,56 +1,108 @@
-//Load Messages
+//Login
 
-function getMessages() {
-  const MessagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-  MessagePromise.then(loadMessages);
+function checkUsername(){
+
+    //Selects the input box:
+    const InputBox = document.querySelector('input');
+
+    //Selects the username inside the box and sets it in the server form:
+    const Username = {"name": InputBox.value};
+
+    //Sends the username to the server:
+    const UsernamePost = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", Username);
+
+    //If success:
+    UsernamePost.then(startChat);
+
+    //If fail:
+    UsernamePost.catch(errorCodes);
+
 }
 
-setInterval(getMessages, 3000);
+function startChat(){
+
+    //Selects the LoginScreen and hides it:
+    const LoginScreen = document.querySelector('.login');
+    LoginScreen.classList.add('hidden');
+
+    //Continues to load messages:
+    getMessages();
+
+    //Starts the update interval:
+    interval();
+
+}
+
+//Load Messages
+function getMessages() {
+
+    //Sends a request to get the messages from the server:
+  const MessagePromise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+  //If successful:
+  MessagePromise.then(loadMessages);
+  
+}
+
+function interval(){
+    //Repeats this function every 3s to keep the chat updated:
+    setInterval(getMessages, 3000);
+}
 
 function loadMessages(Response){
-    //Selects the list containing the messages:
-    let ChatBox = document.querySelector('.chat ul')
+
     //Variable for running through the data contained in the promise according to their index:
     let SelectedIndex = 0;
+
+    //Selects the list containing the messages:
+    let ChatBox = document.querySelector('.chat ul')
+
+    //Clears the chat so it doesn't overaccumulate messages when updated:
+    ChatBox.innerHTML = ``;
 
     //Adds the messages in the chat:
     while (SelectedIndex < Response.data.length){
 
         //Selects the type of the message:
-        let MessageType = Response.data[SelectedIndex].type;
-        //Selects the time of the message:
-        let MessageTime = Response.data[SelectedIndex].time;
-        //Selects the sender:
-        let MessageSender = Response.data[SelectedIndex].from;
-        //Selects the receiver:
-        let MessageReceiver = Response.data[SelectedIndex].to;
-        //Selects the text:
-        let MessageText = Response.data[SelectedIndex].text;
+        const MessageType = Response.data[SelectedIndex].type;
 
+        //Selects the time of the message:
+        const MessageTime = Response.data[SelectedIndex].time;
+
+        //Selects the sender:
+        const MessageSender = Response.data[SelectedIndex].from;
+
+        //Selects the receiver:
+        const MessageReceiver = Response.data[SelectedIndex].to;
+
+        //Selects the text:
+        const MessageText = Response.data[SelectedIndex].text;
+
+        //Text that goes after the sender:
+        let AfterSender = ``;
 
         //Adds the 'li' elements inside the list according to their type:
         if (MessageType == 'status'){
-            ChatBox.innerHTML += `
-                <li class="status">
-                    <h1>
-                        <span>(${MessageTime})</span> <strong>${MessageSender}</strong> ${MessageText}
-                    </h1>
-                </li>`
+
+            AfterSender = `${MessageText}`
+
         }else if (MessageType == 'message'){
-            ChatBox.innerHTML += `
-                <li class="message">
-                    <h1>
-                        <span>(${MessageTime})</span> <strong>${MessageSender}</strong> para <strong>${MessageReceiver}</strong>: ${MessageText}
-                    </h1>
-                </li>`
+
+            AfterSender = `para <strong>${MessageReceiver}</strong>: ${MessageText}`
+
         }else if (MessageType == 'private_message'){
-            ChatBox.innerHTML += `
-                <li class="private-message">
-                    <h1>
-                        <span>(${MessageTime})</span> <strong>${MessageSender}</strong> reservadamente para <strong>${MessageReceiver}</strong>: ${MessageText}
-                    </h1>
-                </li>`
+
+            AfterSender = `reservadamente para <strong>${MessageReceiver}</strong>: ${MessageText}`
+
         }
+
+        //Updates the chat with the new messages:
+        ChatBox.innerHTML += `
+            <li class="${MessageType}">
+                <h1>
+                    <span>(${MessageTime})</span> <strong>${MessageSender}</strong> ${AfterSender}
+                </h1>
+            </li>`
 
         SelectedIndex++
     }
@@ -58,4 +110,14 @@ function loadMessages(Response){
     //Scrolls to the bottom automatically:
     window.scrollTo(0, document.body.scrollHeight);
 
+}
+
+//Error Codes:
+
+function errorCodes(error){
+    const errorCode = error.response.status;
+    if (errorCode === 400){
+        const ErrorText = document.querySelector('h2');
+        ErrorText.innerHTML = 'Já há um usuário online com este nome';
+    }
 }
